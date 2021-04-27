e2525c2177bdd3891d3e1a6d0a3bfbcf9ad6fbe2
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-04-27 10:23:47 +0530

    Initial commit.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd752380d3fd6c286646c0aa7442daf56'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bba88d55-589e-49d0-9057-e5734aa652e2'
+};
